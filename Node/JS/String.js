b1617a085a31c408d110d3e2f4dcb06bd4a170a8
Choigunwoo <gunwoo8873@outlook.kr>
2023-11-30 11:11:19 +0900

    String

---
+++ Node/JS/String.js
@@ -0,0 +1,12 @@
+const Name = 'Choigunwoo'
+const Skill = 'JavaScript'
+
+// EX1
+console.log('EX1');
+console.log(Name);
+console.log(Skill);
+
+console.log('-----------');
+
+// EX2
+console.log(`${Name} , ${Skill}`);
