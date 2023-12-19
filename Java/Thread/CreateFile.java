336c0222a7c6415af346a34c541163ea0c1e9b31
Choigunwoo <gunwoo8873@outlook.kr>
2023-12-20 00:03:13 +0900

    Java CreateFile Text

---
+++ Java/Thread/CreateFile.java
@@ -0,0 +1,20 @@
+package Thread;
+import java.io.*;
+
+public class CreateFile {
+    public static void main(String[] args) {
+        InputStreamReader Reader = new InputStreamReader(System.in);
+
+        FileWriter AddFile = null;
+        int c;
+        try {
+            AddFile = new FileWriter("./CreateFile.text");
+            while ((c = Reader.read())!= 1) {
+                AddFile.write(c);
+            }
+            Reader.close();
+            AddFile.close();
+        }
+        catch (IOException e) { System.out.println("Error"); }
+    }    
+}
