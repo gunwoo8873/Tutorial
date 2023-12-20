514647ca2cc06385344098939feaa35e5d5c8c82
Choigunwoo <gunwoo8873@outlook.kr>
2023-12-20 09:45:12 +0900

    Fix Array Element
    Array Elements Output and SumOutput Add Code

--- Java/Basic/Collection.java
+++ Java/Basic/Collection.java
@@ -15,23 +15,45 @@ public class Collection {
         // add(Index TagetNumber, Change Element)
         Vector.add(3, 150);
 
+        /*
         // Array(Index) Element
         System.out.println("Vector Element : " + Vector.size());
 
         // Element Total Sum Output
         System.out.println("Vector Total Element : " + Vector.capacity());
+        */
 
+        // While or For Code
+        // Iterator All Elements Output
+        Iterator<Integer> Iterator = Vector.iterator();
+        while (Iterator.hasNext()) {
+            int N = Iterator.next();
+            System.out.println(N);
+        }
+        /*
         // Element List Output
         for (int i = 0; i < Vector.size(); i++) {
             int n = Vector.get(i);
             System.out.println(n);
         }
+        */
 
+        // Element Sum Output While or For Code
         int sum = 0;
-        for (int i = 0; i < Vector.size(); i++) {
-            int n = Vector.elementAt(i);
-            sum += n;
+        Iterator = Vector.iterator();
+        while (Iterator.hasNext()) {
+            int N  = Iterator.next();
+            sum += N;
         }
         System.out.println("Vector Sum : " + sum);
+        /*
+        int sum = 0;
+        for (int i = 0; i < Vector.size(); i++) {
+            int N = Vector.elementAt(i);
+            sum += N;
+        }
+        System.out.println("Vector Sum : " + sum);
+        */
+
     }
 }
